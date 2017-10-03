922ebecb5f3de6dee39e8f92125b255456d441e6
Kenny Stepney <kennystepney@hotmail.com>
2017-10-03 18:41:10 -0500

    Update AddCode.java

--- AddCode.java
+++ AddCode.java
@@ -1,1 +1,12 @@
-
+for(int binIndex = 0; binIndex < totalBinCount; binIndex++){
+  int FrameRead = inputFile.readFrames(SampleButter, BIN_SIZE)
+  return Butter[binIndex] = 0;
+  for(int SampleCount = 0; SampleCount< SampleButter.length; SampleCount++){
+    return Butter[binIndex] += Math.abs(SampleButter[sampleCount]);
+  }
+  if(FrameRead < BinSize && !BinIndex == totalBinCount - 1){
+    throw new RuntimeException("short read from WAV tile");
+  }
+  
+  return returnButter;
+}
